379c787de34833be6e3af6d357477cb214ac1e31
Celso Yamashita <ctyamashita@hotmail.com>
2024-01-31 16:59:57 +0900

    wip

--- board.js
+++ board.js
@@ -1,3 +1,17 @@
+// Rand method
+const rand = (size) => {
+  return Math.floor(Math.random() * size);
+}
+
+// Sample method
+Array.prototype.sample = function(size) {
+  if (size > 1) {
+    return this.shuffle().slice(0, size)
+  } else {
+    return this[rand(this.length) - 1]
+  }
+}
+
 class Board {
   constructor(document, height, width) {
     this.height = height;
